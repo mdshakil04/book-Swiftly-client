c1a4a84a873875ac21cff194a69a4a35c56b508e
mdshakil04 <m.shakil04@yahoo.com>
2024-04-02 16:24:12 +0600

    Navbar and Footer added

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Welcome to footer</h2>
+        </div>
+    );
+};
+
+export default Footer;
