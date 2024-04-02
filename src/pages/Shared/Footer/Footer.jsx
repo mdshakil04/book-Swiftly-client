6ca4c58e5e1ac0a7b6c58e63a0a178e5a9616a10
mdshakil04 <m.shakil04@yahoo.com>
2024-04-02 23:57:52 +0600

    Footer Done

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            <h2 className="text-2xl">Welcome to footer</h2>
-        </div>
-    );
+  return (
+    <footer className="footer p-10 bg-[#082250] text-white">
+      <nav>
+        <h6 className="footer-title">Services</h6>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+      <form>
+        <h6 className="footer-title">Newsletter</h6>
+        <fieldset className="form-control w-80">
+          <label className="label">
+            <span className="label-text text-white">Enter your email address</span>
+          </label>
+          <div className="join">
+            <input
+              type="text"
+              placeholder="username@site.com"
+              className="input text-black input-bordered join-item"
+            />
+            <button className="btn btn-primary join-item">Subscribe</button>
+          </div>
+        </fieldset>
+      </form>
+    </footer>
+  );
 };
 
 export default Footer;
