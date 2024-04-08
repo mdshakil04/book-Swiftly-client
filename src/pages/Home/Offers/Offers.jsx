c32f6aaede7bb794dc9a9671eefa08e0eb7116bf
mdshakil04 <m.shakil04@yahoo.com>
2024-04-09 02:46:51 +0600

    Another component added

--- src/pages/Home/Offers/Offers.jsx
+++ src/pages/Home/Offers/Offers.jsx
@@ -6,7 +6,7 @@ const Offers = () => {
         <div className=' my-4'>
             <h1 className='text-3xl text-center border-y-4 max-w-xs container mx-auto'>Travel more, spend less</h1>
             <p className=' text-center mb-4'>Promotions, deals, and special offers for you</p>
-            <div className=' grid md:grid-cols-4 gap-4 p-4'>
+            <div className=' grid lg:grid-cols-4 md:grid-cols-2 gap-4 p-4'>
                 <div className=' border-2 border-blue-900 hover:bg-emerald-100 rounded-3xl p-4'>
                     <h1 className="text-xl">Genius</h1>
                     <p>You are at <span className=' font-bold'>Genius Level 1</span> in<br></br> our loyalty program</p>
