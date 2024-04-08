30410585783a1dbc3f17f5d9ba825dd7a5b01239
mdshakil04 <m.shakil04@yahoo.com>
2024-04-08 23:44:38 +0600

    Banner responsive done

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -7,9 +7,9 @@ const Banner = () => {
   return (
     <div>
       <Parallax className=" pl-24" bgImage={bannerImg}>
-        <div className="hero-content my-36 lg:-ml-96   text-center rounded-3xl">
-          <div className=" bg-base-100 p-12 rounded-3xl">
-            <h1 className=" text-3xl font-bold ">
+        <div className="hero-content lg:my-36 md:-ml-64 -ml-32 text-center rounded-3xl">
+          <div className=" bg-base-100 lg:p-12 rounded-3xl">
+            <h1 className=" lg:text-3xl md:text-2xl text-xl p-4 font-bold ">
               Unlock Unforgettable Experiences,<br></br> Discover Local Wonders,
               <br></br> Create Lifelong Memories
             </h1>
@@ -17,25 +17,6 @@ const Banner = () => {
         </div>
       </Parallax>
     </div>
-    // <div
-    //   className="hero min-h-screen"
-    //   style={{
-    //     backgroundImage: `url(${bannerimg})`,
-    //   }}
-    // >
-    //   <div className="hero-overlay bg-opacity-50"></div>
-    //   <div className="hero-content md:-mt-40 lg:-ml-96 bg-base-100  text-center rounded-3xl">
-    //     <div className="max-w-md">
-    //       <h1 className="mb-5 text-3xl font-bold ">
-    //       Unlock Unforgettable Experiences,<br></br> Discover Local Wonders,<br></br> Create
-    //         Lifelong Memories
-    //       </h1>
-    //       <p className="mb-5">
-
-    //       </p>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
