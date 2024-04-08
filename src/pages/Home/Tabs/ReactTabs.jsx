8769935651c36fb07b8faf2750901c6f1f3ae26e
mdshakil04 <m.shakil04@yahoo.com>
2024-04-09 00:41:46 +0600

    React tab updated

--- src/pages/Home/Tabs/ReactTabs.jsx
+++ src/pages/Home/Tabs/ReactTabs.jsx
@@ -2,6 +2,15 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import DhakaImage1 from "../../../assets/Images/Dhaka/friends-walking-up-mountain.jpg";
+import DhakaImage2 from "../../../assets/Images/Dhaka/tourists-taking-stroll-around-hija-glamping-lake-bloke-nova-vas-slovenia.jpg";
+import DhakaImage3 from "../../../assets/Images/Dhaka/weekend-seniors.jpg";
+import BarishalImage1 from "../../../assets/Images/Barishal/girl-embroidered-dress-is-standing-yard-looking-into-sky.jpg";
+import BarishalImage2 from "../../../assets/Images/Barishal/medium-shot-couple-hiking-together.jpg";
+import BarishalImage3 from "../../../assets/Images/Barishal/shruti-singh-zk1KY3GQEZA-unsplash.jpg";
+import Noakhali1 from "../../../assets/Images/Noakhali/etty-fidele-oJpkjWcScyg-unsplash.jpg";
+import Noakhali2 from "../../../assets/Images/Noakhali/rani-george-nXOgb54BMB4-unsplash.jpg";
+import Noakhali3 from "../../../assets/Images/Noakhali/rice-noodles-bowl-curry-paste-with-chili-cucumber-long-bean-lime-garlic-spring-onion.jpg";
 
 const ReactTabs = () => {
   return (
@@ -14,35 +23,54 @@ const ReactTabs = () => {
           <Tab>Dhaka</Tab>
           <Tab>Barishal</Tab>
           <Tab>Noakhali</Tab>
-          <Tab>Chittagong</Tab>
         </TabList>
 
         <TabPanel>
-          <div className=" grid lg:grid-cols-3 md:grid-cols-2 gap-8 ">
-            <h2 className=" border-2 bg-lime-200 h-[350px]">Raz-Bari</h2>
-            <h2 className=" border-2 bg-emerald-200 h-[350px]">Narayongonj</h2>
-            <h2 className=" border-2 bg-cyan-300 h-[350px]">Raz-Bari</h2>
+          <div className=" grid lg:grid-cols-3 md:grid-cols-2 gap-4 ">
+            <div>
+                <h1 className=" p-4 font-bold text-2xl text-blue-950 absolute">Naraongonj</h1>
+                <img className=" md:h-[300px]" src={DhakaImage1} alt="Image" />
+            </div>
+            <div>
+                <h1 className=" p-4 font-bold text-2xl text-white absolute">Foridpur</h1>
+                <img className=" md:h-[300px]" src={DhakaImage2} alt="Image" />
+            </div>
+            <div>
+                <h1 className=" p-4 font-bold text-2xl text-white absolute">Chadpur</h1>
+                <img className=" md:h-[300px]" src={DhakaImage3} alt="Image" />
+            </div>
           </div>
         </TabPanel>
         <TabPanel>
-        <div className=" grid lg:grid-cols-3 md:grid-cols-2 gap-8 ">
-            <h2 className=" border-2 bg-cyan-300 h-[350px]">Bhola</h2>
-            <h2 className=" border-2 bg-lime-200 h-[350px]">Potuakhali</h2>
-            <h2 className=" border-2 bg-emerald-200 h-[350px]">Gopalgonj</h2>
+          <div className=" grid lg:grid-cols-3 md:grid-cols-2 gap-4 ">
+          <div>
+                <h1 className=" p-4 font-bold text-2xl text-white absolute">Bhola</h1>
+                <img className=" md:h-[300px]" src={BarishalImage1} alt="Image" />
+            </div>
+            <div>
+                <h1 className=" p-4 font-bold text-2xl text-white absolute">Potuakhali</h1>
+                <img className=" md:h-[300px]" src={BarishalImage2} alt="Image" />
+            </div>
+            <div>
+                <h1 className=" p-4 font-bold text-2xl text-white absolute">FhirozPur</h1>
+                <img className=" md:h-[300px]" src={BarishalImage3} alt="Image" />
+            </div>
           </div>
         </TabPanel>
         <TabPanel>
-        <div className=" grid lg:grid-cols-3 md:grid-cols-2 gap-8 ">
-            <h2 className=" border-2 bg-emerald-200 h-[350px]">Raz-Bari</h2>
-            <h2 className=" border-2 bg-lime-200 h-[350px]">Narayongonj</h2>
-            <h2 className=" border-2 bg-cyan-300 h-[350px]">Raz-Bari</h2>
-          </div>
-        </TabPanel>
-        <TabPanel>
-        <div className=" grid lg:grid-cols-3 md:grid-cols-2 gap-8 ">
-            <h2 className=" border-2 bg-emerald-200 h-[350px]">Raz-Bari</h2>
-            <h2 className=" border-2 bg-cyan-300 h-[350px]">Narayongonj</h2>
-            <h2 className=" border-2 bg-lime-200 h-[350px]">Raz-Bari</h2>
+          <div className=" grid lg:grid-cols-3 md:grid-cols-2 gap-4 ">
+          <div>
+                <h1 className=" p-4 font-bold text-2xl text-white absolute">Maizdi</h1>
+                <img className=" md:h-[300px]" src={Noakhali1} alt="Image" />
+            </div>
+            <div>
+                <h1 className=" p-4 font-bold text-2xl text-white absolute">LoxmiPur</h1>
+                <img className=" md:h-[300px]" src={Noakhali2} alt="Image" />
+            </div>
+            <div>
+                <h1 className=" p-4 font-bold text-2xl text-white absolute">Laxsham</h1>
+                <img className=" md:h-[300px]" src={Noakhali3} alt="Image" />
+            </div>
           </div>
         </TabPanel>
       </Tabs>
