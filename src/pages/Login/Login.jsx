73767c19f5180d44adc676c30b4436a245bc72cc
mdshakil04 <m.shakil04@yahoo.com>
2024-04-15 02:18:24 +0600

    Login image added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,52 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-
+import React from "react";
+import loginImg from "../../assets/Images/Festival/login.jpg";
 const Login = () => {
-    return (
-        <div>
-            <h2>Please! Login</h2>
+  return (
+    <div>
+      <div className="hero ">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <img src={loginImg} alt="" />
+          </div>
+          <div className="card shrink-0 w-full max-w-xl shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
