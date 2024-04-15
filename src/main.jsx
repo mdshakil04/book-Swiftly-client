fbf806e6b6d09d4b216be00ddaf9e5f9daa849bb
mdshakil04 <m.shakil04@yahoo.com>
2024-04-15 21:17:18 +0600

    Firebase AuthProvider created

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
+import AuthProvider from "./providers/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="  ">
-      <RouterProvider router={router} />
-    </div>
+    <AuthProvider>
+      <div className="  ">
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
   </React.StrictMode>
 );
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { useState } from 'react';
+import { createContext } from 'react';
+import { getAuth } from "firebase/auth";
+import { app } from '../firebase/firebase.config';
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const authInfo = {
+        user, 
+        loading
+    }
+    return (
+        <AuthContext.Provider value ={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
