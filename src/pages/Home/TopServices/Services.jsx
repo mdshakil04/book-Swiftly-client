e91da362a3a5f31ee05a38640063ca215d708833
mdshakil04 <m.shakil04@yahoo.com>
2024-04-24 12:58:35 +0600

    Footer component changed

--- src/pages/Home/TopServices/Services.jsx
+++ src/pages/Home/TopServices/Services.jsx
@@ -5,7 +5,7 @@ import TopServices from "./TopServices";
 const Services = () => {
   const [services, setServices] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/services")
+    fetch("services.json")
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, []);
