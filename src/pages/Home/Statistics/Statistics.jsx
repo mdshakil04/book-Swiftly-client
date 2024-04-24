a915e0ae109aa970ea46be5f4d406cb8cf95c89e
mdshakil04 <m.shakil04@yahoo.com>
2024-04-24 11:00:50 +0600

    Statistics component done

--- src/pages/Home/Statistics/Statistics.jsx
+++ src/pages/Home/Statistics/Statistics.jsx
@@ -1,11 +1,34 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-
+import { MdOutlineFreeCancellation } from "react-icons/md";
+import { FaPushed } from "react-icons/fa";
+import { TbReservedLine } from "react-icons/tb";
+import { MdOutlineReviews } from "react-icons/md";
 const Statistics = () => {
     return (
-        <div className=' my-12'>
+        <div className=' my-12 border p-12'>
             <h2 className=' text-4xl text-center'>Why book with Book-Swiftly?</h2>
-            <div>
-
+            <div className=' mt-8 grid grid-cols-4 gap-4 '>
+                <div className=' flex flex-col items-center gap-2 p-4 rounded-lg bg-[#EAFBF7]'>
+                    <p className=' text-4xl'><MdOutlineFreeCancellation /></p>
+                    <h2 className='text-3xl'>Free Cancellation</h2>
+                    <p>Stay flexible on your trip.</p>
+                </div>
+                <div className=' flex flex-col items-center gap-2 p-4  rounded-lg bg-[#EAFBF7]'>
+                    <p className=' text-4xl'><FaPushed /></p>
+                    <h2 className='text-3xl'>100,000+ experiences</h2>
+                    <p>Make memories around the world.</p>
+                </div>
+                <div className=' flex flex-col items-center gap-2 p-4  rounded-lg bg-[#EAFBF7]'>
+                    <p className=' text-4xl'><TbReservedLine /></p>
+                    <h2 className='text-3xl'>Reserve now, pay later</h2>
+                    <p>Book your spot.</p>
+                </div>
+                <div className=' flex flex-col items-center gap-2 p-4  rounded-lg bg-[#EAFBF7]'>
+                    <p className=' text-4xl'><MdOutlineReviews /></p>
+                    <h2 className='text-3xl'>Trusted reviews</h2>
+                    <p>4.3 stars from 140,000+ reviews.</p>
+                </div>
             </div>
         </div>
     );
