07c4d9d94189d874b301664e009a961f3a4f298f
mdshakil04 <m.shakil04@yahoo.com>
2024-04-26 11:13:53 +0600

    Error page updated

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import errImg from "../../assets/Images/Festival/error.svg"
 
 const Error = () => {
     return (
         <div className=' flex flex-col justify-center items-center'>
-            <h2 className=' text-9xl text-red-400 text-center'>404 Not Found</h2>
-            <li className=' btn btn-outline btn-warning'>
+            <img className=' w-8/12' src={errImg} alt="error" />
+            <a className=' btn btn-outline btn-warning'>
                 <Link to='/'>Go to Home</Link>
-            </li>
+            </a>
         </div>
     );
 };
