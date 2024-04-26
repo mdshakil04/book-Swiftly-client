8632139d45d66cba1e5a067231fa76a7040dbd45
mdshakil04 <m.shakil04@yahoo.com>
2024-04-26 20:06:42 +0600

    Transport Component Done

--- src/pages/Home/MainServices/Transportation/Transportation.jsx
+++ src/pages/Home/MainServices/Transportation/Transportation.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import rikshaw from "../../../../assets/Images/Transportation/Rikshaw.jpg";
+import GorurGari from "../../../../assets/Images/Transportation/GorurGari.jpg";
+import Boat from "../../../../assets/Images/Transportation/Boat.jpg";
+import Van from "../../../../assets/Images/Transportation/Van.jpg";
 
 const Transportation = () => {
   return (
@@ -12,12 +15,13 @@ const Transportation = () => {
         Reliable, Comfortable, and Convenient Travel Solutions for Your
         Adventure!
       </p>
-      <div>
+      <div className=" md:grid lg:grid-cols-4 md:grid-cols-2 gap-8">
         <div className="card w-96 bg-base-100 shadow-xl">
           <figure>
             <img
+              className=" rounded-t-2xl h-[250px]"
               src={rikshaw}
-              alt="Shoes"
+              alt="Image"
             />
           </figure>
           <div className="card-body">
@@ -25,11 +29,68 @@ const Transportation = () => {
               Rikshaw
               <div className="badge badge-secondary">Trending</div>
             </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <div className="badge badge-outline">Fashion</div>
-              <div className="badge badge-outline">Products</div>
-            </div>
+            <p>Rikshaw is a two-wheeler that is used for transportation. It is
+                a two-wheeler that is used for transportation.</p>
+            <p>Capacity: Typically Seats 1-2 person</p>
+            <p>Feature: Open-air seating, pedal power transportation</p>
+            <button  className="btn btn-outline btn-info hover:shadow-lg hover:shadow-blue-400">Book Now</button>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img className=" rounded-t-2xl h-[250px]"
+              src={Van}
+              alt="Image"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+                Ven
+              <div className="badge badge-secondary">Trending</div>
+            </h2>
+            <p>Rikshaw is a two-wheeler that is used for transportation. It is
+                a two-wheeler that is used for transportation.</p>
+            <p>Capacity: Typically Seats 2-4 person</p>
+            <p>Feature: Open-air seating, pedal power transportation</p>
+            <button  className="btn btn-outline btn-info hover:shadow-lg hover:shadow-blue-400">Book Now</button>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img className=" rounded-t-2xl h-[250px]"
+              src={GorurGari}
+              alt="Image"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Gorur Gari
+              <div className="badge badge-secondary">Trending</div>
+            </h2>
+            <p>Rikshaw is a two-wheeler that is used for transportation. It is
+                a two-wheeler that is used for transportation.</p>
+            <p>Capacity: Typically Seats 4-5 person</p>
+            <p>Feature: Open-air seating, pedal power transportation</p>
+            <button  className="btn btn-outline btn-info hover:shadow-lg hover:shadow-blue-400">Book Now</button>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img className=" rounded-t-2xl h-[250px]"
+              src={Boat}
+              alt="Image"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Boat
+              <div className="badge badge-secondary">Trending</div>
+            </h2>
+            <p>Rikshaw is a two-wheeler that is used for transportation. It is
+                a two-wheeler that is used for transportation.</p>
+            <p>Capacity: Typically Seats 8-10 person</p>
+            <p>Feature: Open-air seating, pedal power transportation</p>
+            <button  className="btn btn-outline btn-info hover:shadow-lg hover:shadow-blue-400">Book Now</button>
           </div>
         </div>
       </div>
