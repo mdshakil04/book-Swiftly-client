9b6a3b4434097f39d14741d57eaaf2e6a59d1807
mdshakil04 <m.shakil04@yahoo.com>
2024-04-26 13:05:24 +0600

    Rikshaw added in transport component

--- src/pages/Home/MainServices/Transportation/Transportation.jsx
+++ src/pages/Home/MainServices/Transportation/Transportation.jsx
@@ -1,15 +1,38 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import rikshaw from "../../../../assets/Images/Transportation/Rikshaw.jpg";
 
 const Transportation = () => {
   return (
     <div className=" my-8">
       <h1 className="text-3xl text-center border-y-4 max-w-xs container py-4 mx-auto">
-        Embark on Adventure
+        Our Transport Services
       </h1>
       <p className=" text-center mb-4">
-        Celebrate Culture, Discover Wonders, with Our Team of Expert Guides!
+        Reliable, Comfortable, and Convenient Travel Solutions for Your
+        Adventure!
       </p>
+      <div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src={rikshaw}
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Rikshaw
+              <div className="badge badge-secondary">Trending</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">Fashion</div>
+              <div className="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
