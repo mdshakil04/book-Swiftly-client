1dce266059518000099ff22ce1d30c96230594ba
mdshakil04 <m.shakil04@yahoo.com>
2024-05-05 21:56:10 +0600

    Users API created

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l8rzo73.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +27,9 @@ async function run() {
     // Services collection
     const serviceCollection = client.db("BookSwiftlyDB").collection("services");
     const mainServiceCollection = client.db("BookSwiftlyDB").collection("mainServices");
+    const cartCollection = client.db("BookSwiftlyDB").collection("carts");
+    const userCollection = client.db("BookSwiftlyDB").collection("users");
+    // Services collection
     app.get('/services', async(req, res) =>{
         const result = await serviceCollection.find().toArray();
         res.send(result);
@@ -36,6 +39,31 @@ async function run() {
         const result = await mainServiceCollection.find().toArray();
         res.send(result);
     })
+    // Get cart data
+    app.get('/carts', async(req, res) =>{
+        const result = await cartCollection.find().toArray();
+        res.send(result);
+    })
+    // Cart Collection
+    app.post ('/carts', async(req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    })
+    // Users related API
+    app.post('/users', async(req,res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+    // Delete cart items
+    app.delete('/carts/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
